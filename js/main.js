aaf8c902a5b8807fea813e8b6211774421a22ece
Amirreza Ghajar <mypersonal048@gmail.com>
2018-12-16 01:28:46 +0330

    first commit

---
+++ js/main.js
@@ -0,0 +1,73 @@
+
+function main() {
+
+(function () {
+   'use strict';
+   
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 50
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    });
+
+	// Hide nav on click
+  $(".navbar-nav li a").click(function (event) {
+    // checking drop down menu
+    var toggle = $(".navbar-toggle").is(":visible");
+    if (toggle) {
+      $(".navbar-collapse").collapse('hide');
+    }
+  });
+	
+		
+}());
+
+
+}
+main();
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("to-top").style.display = "block";
+  } else {
+    document.getElementById("to-top").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+//Loading fakeloader
+
+$(".fakeloader").fakeLoader({
+
+timeToHide:1500,
+
+zIndex:"2000",
+
+spinner:"spinner7",
+
+bgColor:"rgba(255, 94, 0, 0.96)"
+
+});
